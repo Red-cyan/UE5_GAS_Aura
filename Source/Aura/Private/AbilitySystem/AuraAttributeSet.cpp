f452c609a86b7f614e7ecd02e43ad4340851bda6
Rcyan <3148764147@qq.com>
2026-02-13 20:20:44 +0800

    添加CurveTable来按照等级控制增加的数值

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -50,15 +50,20 @@ void UAuraAttributeSet::SetEffectProperties(const FGameplayEffectModCallbackData
 	
 	if (IsValid(Props.SourceASC) && Props.SourceASC->AbilityActorInfo.IsValid() && Props.SourceASC->AbilityActorInfo->AvatarActor.IsValid())
 	{
+		// 1. 拿 Avatar (表现Actor)
 		Props.SourceAvatarActor = Props.SourceASC->AbilityActorInfo->AvatarActor.Get();
+		// 2. 尝试从 AbilityActorInfo 拿 Controller
 		Props.SourceController = Props.SourceASC->AbilityActorInfo->PlayerController.Get();
+		// 3. 【关键补救逻辑】如果上面没拿到 Controller，但有 Avatar
 		if (Props.SourceController == nullptr && Props.SourceAvatarActor != nullptr)
 		{
+			// 尝试把 Avatar 转成 Pawn，然后通过 Pawn 拿 Controller
 			if (const APawn* Pawn = Cast<APawn>(Props.SourceAvatarActor))
 			{
 				Props.SourceController = Pawn->GetController();
 			}
 		}
+		// 4. 如果拿到了 Controller，就能拿到 Character
 		if (Props.SourceController)
 		{
 			Props.SourceCharacter = Cast<ACharacter>(Props.SourceController->GetPawn());
@@ -67,7 +72,8 @@ void UAuraAttributeSet::SetEffectProperties(const FGameplayEffectModCallbackData
 	
 	if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
 	{
-		 Props.TargetAvatarActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
+		// 直接从 Data.Target 里拿 Avatar, Controller, Character和ASC
+		Props.TargetAvatarActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
 		Props.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
 		Props.TargetCharacter = Cast<ACharacter>(Props.TargetAvatarActor);
 		Props.TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Props.TargetAvatarActor);
