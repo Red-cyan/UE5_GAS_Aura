058cf1a35064ffbab4079be800f046beb322c8cf
Rcyan <3148764147@qq.com>
2026-02-14 15:11:22 +0800

    广播AssetTag

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -13,10 +13,8 @@ void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* Ability
 {
 	FGameplayTagContainer TagContainer;
 	EffectSpec.GetAllAssetTags(TagContainer);
-	for (const FGameplayTag& Tag : TagContainer)
-	{
-		//给widget控制器广播Tag
-		const FString Msg = FString::Printf(TEXT("GE Tag:%s"),*Tag.ToString());
-		GEngine->AddOnScreenDebugMessage(-1,8.f,FColor::Blue,Msg);
-	}
+	
+		EffectAssetTags.Broadcast(TagContainer);
+	
+	
 }
