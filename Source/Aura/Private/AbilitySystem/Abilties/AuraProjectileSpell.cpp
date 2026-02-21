7ca43a3676363046aaf6ef7f017b0734cbf9f4b4
Rcyan <3148764147@qq.com>
2026-02-21 11:29:33 +0800

    重写ActivateAbility函数

--- Source/Aura/Private/AbilitySystem/Abilties/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilties/AuraProjectileSpell.cpp
@@ -3,3 +3,13 @@
 
 #include "AbilitySystem/Abilties/AuraProjectileSpell.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	UKismetSystemLibrary::PrintString(this,FString("ActivateAbility(C++)"),true,true,FLinearColor::Yellow,3);
+}
