1ee2ba48574c9551a794041a97b3e46aaa63469e
Rcyan <3148764147@qq.com>
2026-02-24 20:17:52 +0800

    创建自定义Gameplay Effect Context类

---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct  FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+	bool IsCriticalHit() const{ return bIsCriticalHit; }
+	bool IsBlockHit() const{ return bIsBlockHit; }
+	
+	void SetCriticalHit(bool bInIsCriticalHit){ bIsCriticalHit = bInIsCriticalHit; }
+	void SetBlockHit(bool bInIsBlockHit){ bIsBlockHit = bInIsBlockHit; }
+	
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return FGameplayEffectContext::StaticStruct();
+	};
+	
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) ;
+protected:
+	
+	UPROPERTY()
+	bool bIsBlockHit = false;
+	
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+
+	
+	
+};
