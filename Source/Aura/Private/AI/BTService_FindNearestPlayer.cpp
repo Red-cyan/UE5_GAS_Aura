e73f24ff344360a2e2fb7b7edf178c3bfff10f52
Rcyan <3148764147@qq.com>
2026-02-28 17:27:54 +0800

    Feat: 引入黑板键选择器并初步实现基于 Tag 的索敌逻辑
    - BTService_FindNearestPlayer: 在头文件中新增了 TargetToFollowSelector 和 DistanceToTargetSelector 两个 FBlackboardKeySelector，用于在蓝图中绑定黑板变量。
    - BTService_FindNearestPlayer: 在 TickNode 中移除了之前的 Debug 打印，新增了动态识别阵营的逻辑（通过判断自身是否有 "Player" Tag 来决定搜寻 "Enemy" 还是 "Player"）。
    - BTService_FindNearestPlayer: 引入 UGameplayStatics::GetAllActorsWithTag 收集所有对应 Tag 的潜在目标 Actor，为下一步计算最短距离做准备。
    - Blueprints: 更新了 BB_EnemyBlackboard (黑板资产) 以及 Aura 相关的角色蓝图，适配新的寻敌系统框架。

--- Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -4,13 +4,17 @@
 #include "AI/BTService_FindNearestPlayer.h"
 
 #include "AIController.h"
+#include "Kismet/GameplayStatics.h"
 
 void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 
-	GEngine->AddOnScreenDebugMessage(1,1.F,FColor::Red,*AIOwner->GetName());
-	GEngine->AddOnScreenDebugMessage(2,1.F,FColor::Green,*ActorOwner->GetName());
+	APawn* OwingPawn= AIOwner->GetPawn();
+	
+	const FName TargetTag = OwingPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
+	TArray<AActor*> ActorsWithTag;
+	UGameplayStatics::GetAllActorsWithTag(OwingPawn,TargetTag,ActorsWithTag);
 	
 	
 }
